0fff702b1e59e1194cdf4c1c8698479177fc170b
Abhi <pandeyabhi142002@gmail.com>
2023-02-02 23:39:28 +0530

    Testing some changes from my side

--- src/main/java/com/driver/model/User.java
+++ src/main/java/com/driver/model/User.java
@@ -7,7 +7,7 @@ import java.util.List;
 @Entity
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String phoneNumber;
